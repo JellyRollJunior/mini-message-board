6529089e6271438b7c90893684d248bc827cc864
Brandon Lin <brandonlin9716@gmail.com>
2025-02-12 21:38:09 +0800

    Extract messages db into db.js

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,16 +1,4 @@
-const messages = [
-    {
-        text: 'Hi there!',
-        user: 'Amando',
-        added: new Date(),
-    },
-    {
-        text: 'Hello World!',
-        user: 'Charles',
-        added: new Date(),
-    },
-];
-
+const messages = require('../db.js');
 const { Router } = require('express');
 const indexRouter = Router();
 
