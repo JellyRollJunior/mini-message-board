652af07c383f09e6bace4e2ec1228fe6e8d8fa9a
Brandon Lin <brandonlin9716@gmail.com>
2025-02-12 17:19:48 +0800

    Create index route and basic server

---
+++ routes/indexRouter.js
@@ -0,0 +1,21 @@
+const messages = [
+    {
+        text: 'Hi there!',
+        user: 'Amando',
+        added: new Date(),
+    },
+    {
+        text: 'Hello World!',
+        user: 'Charles',
+        added: new Date(),
+    },
+];
+
+const { Router } = require('express');
+const indexRouter = Router();
+
+indexRouter.get('/', (req, res) => {
+    res.send('hello world!');
+});
+
+module.exports = indexRouter;
