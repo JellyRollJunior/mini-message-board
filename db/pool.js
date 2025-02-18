c6f190dc91a1d90d4bec8a393120f1dfc5bae02c
Brandon Lin <brandonlin9716@gmail.com>
2025-02-18 11:48:27 +0800

    Connect to remote railway db

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,5 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
+    connectionString: `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
 });
